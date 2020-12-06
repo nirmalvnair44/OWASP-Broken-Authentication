43d8d534914d1f1bbfbd6b0fafbf7d09230d01c1
nirmalvnair44 <72847141+nirmalvnair44@users.noreply.github.com>
2020-12-06 14:18:51 +0530

    Update app.js

--- nodejs/app.js
+++ nodejs/app.js
@@ -4,7 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const dotenv = require('dotenv');
 const session = require('express-session');
-
+var validator = require('express-validator');
 dotenv.config({ path: './.env'});
 
 const app = express();
@@ -22,12 +22,14 @@ app.use(express.static(publicdir));
 
 //To parse url encoded bodies
 app.use(express.urlencoded({extended:false}));
+//app.use(validator());      //
 //to parse json bodies
 app.use(express.json());
 //cookieParser used to set up cookies in our browser
 app.use(cookieParser());    //use this to manage cookies, terminate sessions
-
+//app.use(session({secret : }))
 app.set('view engine', 'hbs');
+
 
 //CONNECT TO THE MYSQL DATABASE
 db.connect( (error) => {
@@ -42,6 +44,7 @@ db.connect( (error) => {
 //Define Routes
 
 app.use('/', require('./routes/pages'));    
+app.use('/loggedin', require('./routes/pages'));
 
 app.use('/auth', require('./routes/auth'))
 
